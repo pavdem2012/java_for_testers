46efa400f0b461c1f8db7ef77ea5da2c0471b1fb
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-07-19 20:58:57 +0700

    add class Geometry & void function

---
+++ sanbox/src/main/java/Geometry.java
@@ -0,0 +1,7 @@
+public class Geometry {
+    public static void main(String[] args) {
+        var side = 7.;
+        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+
+    }
+}
