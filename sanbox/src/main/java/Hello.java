8df513da29f4df24e0982761cbce4ec0550362ba
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-07-26 16:36:54 +0700

    added code that causes exceptions

--- sanbox/src/main/java/Hello.java
+++ sanbox/src/main/java/Hello.java
@@ -2,7 +2,15 @@ import java.io.File;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        try {
+            int z = calculate();
+            System.out.println(z);
+            System.out.println("Hello world!!!");
+        } catch (ArithmeticException exception) {
+            exception.printStackTrace();
+        }
+
+
         System.out.println(2 + 2);
         System.out.println(2 * 2);
         System.out.println(2 - 2);
@@ -18,5 +26,18 @@ public class Hello {
         System.out.println(configFile.getAbsoluteFile());
 
         System.out.println(configFile.exists());
+    }
+
+    private static int calculate() {
+        var x = 1;
+        var y = 1;
+        //var y = 0;
+        int z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
     }
 }
