d13b0a0f516c24de0df78078fab2d4a58304e156
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-07-26 17:31:20 +0700

    Added triangle checking conditions to the triangle class and tests to the triangle test class

--- sanbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sanbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,6 +1,18 @@
 package ru.stqa.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side length must be non-negative");
+        }
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Triangle inequality violated");
+        }
+    }
+
+
+
     public static void printArea(Triangle t) {
         System.out.printf("Площадь треугольника со сторонами %f, %f и %f = %f%n",
                 t.a, t.b, t.c, t.area());
--- sanbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
+++ sanbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
@@ -18,4 +18,81 @@ public class TriangleTest {
         Assertions.assertEquals(6.0, triangle.area());
     }
 
+    @Test
+    void cannotCreateTriangleWithNegativeASide() {
+        try {
+            new Triangle(-3.0, 4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeBSide() {
+        try {
+            new Triangle(3.0, -4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeCSide() {
+        try {
+            new Triangle(3.0, 4.0, -5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSides() {
+        try {
+            new Triangle(-3.0, -4.0, -5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateInvalidTriangle1() {
+        try {
+            new Triangle(1.0, 2.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateInvalidTriangle2() {
+        try {
+            new Triangle(5.0, 2.0, 1.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateInvalidTriangle3() {
+        try {
+            new Triangle(2.0, 5.0, 1.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void canCreateEquilateralTriangle() {
+        var triangle = new Triangle(2.0, 2.0, 2.0);
+        Assertions.assertEquals(3.0, triangle.perimeter() / 2);
+    }
+
+
 }
