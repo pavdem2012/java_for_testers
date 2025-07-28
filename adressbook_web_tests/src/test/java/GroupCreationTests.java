9b1278540a3a0fd4cc7c82f2ce2c167beffb0d25
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-07-28 10:41:52 +0700

    Add fixtures and preconditions

---
+++ adressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GroupCreationTests {
+    private static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/index.php");
+            driver.manage().window().setSize(new Dimension(2360, 1321));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    @Test
+    public void canCreateGroup() throws InterruptedException {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.name("new")).click();
+        Thread.sleep(300);
+        driver.findElement(By.name("group_name")).sendKeys("group name");
+        driver.findElement(By.name("group_header")).sendKeys("group header");
+        driver.findElement(By.name("group_footer")).sendKeys("group footer");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+    @Test
+    public void canCreateGroupWithEmptyName() throws InterruptedException {
+
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.name("new")).click();
+        Thread.sleep(300);
+        driver.findElement(By.name("group_name")).sendKeys("");
+        driver.findElement(By.name("group_header")).sendKeys("");
+        driver.findElement(By.name("group_footer")).sendKeys("");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
+}
