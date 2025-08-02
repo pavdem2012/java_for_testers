4c5881a2c25fa7a475a4d0c0028cab6136699464
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-08-02 17:41:14 +0700

    add the ability to parameterize tests in Firefox and Chrome browsers.

--- adressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ adressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -3,6 +3,7 @@ package manager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
     protected WebDriver driver;
@@ -11,9 +12,15 @@ public class ApplicationManager {
     private ContactsHelper contacts;
 
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/index.php");
             driver.manage().window().setSize(new Dimension(2360, 1321));
