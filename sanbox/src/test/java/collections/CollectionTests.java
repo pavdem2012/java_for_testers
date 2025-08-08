05585b3fe8953e454a38c4bc40ff7d7c6e368748
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-08-08 13:34:26 +0700

    add List element

--- sanbox/src/test/java/collections/CollectionTests.java
+++ sanbox/src/test/java/collections/CollectionTests.java
@@ -3,16 +3,34 @@ package collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionTests {
     @Test
-    void arrayTests(){
-        var array = new String[] {"a","b","c"};
+    void arrayTests() {
+        //var array = new String[]{"a", "b", "c"};
+        var array = new String[3];
+        Assertions.assertEquals(3, array.length);
+        array[0] = "a";
         Assertions.assertEquals("a", array[0]);
 
         array[0] = "d";
         Assertions.assertEquals("d", array[0]);
+    }
+    @Test
+    void listTests(){
+        //var list = List.of("a", "b", "c"); //немодифицированный
+        //var list  = new ArrayList<String>();
+        var list  = new ArrayList<>(List.of("a", "b", "c"));
+        Assertions.assertEquals(3, list.size());
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        Assertions.assertEquals(6, list.size());
+        Assertions.assertEquals("a", list.get(0));
+        list.set(0,"d");
+        Assertions.assertEquals("d", list.get(0));
 
     }
-
-
 }
