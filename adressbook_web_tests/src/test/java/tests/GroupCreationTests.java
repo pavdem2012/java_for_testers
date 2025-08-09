0923cefa27ce687ce191e5d38b1183b1ab3e403a
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-08-09 11:13:46 +0700

    reformat tests in GroupCreationTests class to ParameterizedTest w Data Provider

--- adressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ adressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -2,38 +2,71 @@ package tests;
 
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void canCreateGroup() throws InterruptedException {
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>();
+        for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            result.add(new GroupData(randomString(i + 10), randomString(i + 10), randomString(i + 10)));
+        }
+        return result;
+    }
+
+/*    @ParameterizedTest
+    @ValueSource(strings = {"group name", "group name'"})
+    public void canCreateGroup(String name) throws InterruptedException {
         int groupCont = app.groups().getCount();
-        app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCont = app.groups().getCount();
         Assertions.assertEquals(groupCont + 1, newGroupCont);
 
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void canCreateGroupWithEmptyName() throws InterruptedException {
         app.groups().createGroup(new GroupData());
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void canCreateGroupWithNameOnly() throws InterruptedException {
         app.groups().createGroup(new GroupData().withName("some name"));
-    }
+    }*/
+public static List<GroupData> negativeGroupProvider() {
+    return new ArrayList<>(List.of(
+            new GroupData("group name'", "", "")));
+}
 
-    @Test
-    public void canCreateMultipleGroups() throws InterruptedException {
-        int n =5;
+    @ParameterizedTest
+    @MethodSource("groupProvider")
+    public void canCreateMultipleGroups(GroupData group) throws InterruptedException {
         int groupCont = app.groups().getCount();
-        for (int i = 0; i < n; i++ ) {
-            app.groups().createGroup(new GroupData(randomString(i+1), "group header", "group footer"));
-        }
-        int newGroupCont = app.groups().getCount();
-        Assertions.assertEquals(groupCont + n, newGroupCont);
 
+        app.groups().createGroup(group);
+
+        int newGroupCont = app.groups().getCount();
+
+        Assertions.assertEquals(groupCont + 1, newGroupCont);
+
+    }
+    @ParameterizedTest
+    @MethodSource("negativeGroupProvider")
+    public void canNotCreateMultipleGroups(GroupData group) throws InterruptedException {
+        int groupCont = app.groups().getCount();
+        app.groups().createGroup(group);
+        int newGroupCont = app.groups().getCount();
+        Assertions.assertEquals(groupCont , newGroupCont);
     }
 }
