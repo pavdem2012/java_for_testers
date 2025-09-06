f7d85c58d70fc49a7ecacae3167defebcf6d9712
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-09-06 13:26:32 +0700

    Added code from the lecture James Mail Server and Command Line Interface Management

---
+++ mantis_tests/src/test/java/tests/JamesTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import common.CommonFunctions;
+import org.junit.jupiter.api.Test;
+
+public class JamesTests extends TestBase{
+    @Test
+    void canCreateUser() {
+        app.jamesCli().addUser(
+                String.format("%s@localhost", CommonFunctions.randomString(8)),
+                "password");
+    }
+}
