8afb7e3eda8d1747281f2f78fd32c3eb821ee963
Павел Олегович <78712643+pavdem2012@users.noreply.github.com>
2025-09-06 14:05:32 +0700

    исправлен JamesCliHelper

--- mantis_tests/src/test/java/manager/JamesCliHelper.java
+++ mantis_tests/src/test/java/manager/JamesCliHelper.java
@@ -9,15 +9,16 @@ public class JamesCliHelper extends HelperBase{
     public void addUser(String email, String password) {
         CommandLine cmd = new CommandLine(
                 "java",
+                "-Dworking.directory=" + manager.property("james.workingDir"),
                 "-cp",
-                "\"james-server-jpa-app.lib/*\"",
-                "org.apache.james.cli.ServerCMD",
-                "AddUser", email, password);
+                "james-server-jpa-app.jar:james-server-jpa-app.lib/*",
+                "org.apache.james.cli.ServerCmd",
+                "AddUser",  email, password);
         cmd.setWorkingDirectory(manager.property("james.workingDir"));
         CircularOutputStream out = new CircularOutputStream();
         cmd.copyOutputTo(out);
         cmd.execute();
         cmd.waitFor();
-        System.out.println(out);
+        System.out.println("Command output: " + out);
     }
 }
